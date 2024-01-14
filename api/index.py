6c4b2d1869b42e4465ac84a8184bb89b6a7211b0
Rahul <saihanumarahul66@gmail.com>
2024-01-15 02:01:22 +0530

    fix: deployment for mylistanalyzerdash

--- api/index.py
+++ api/index.py
@@ -49,8 +49,8 @@ class MainApplication:
 
 
 Application = MainApplication()
+Application.app.renderer = (pathlib.Path(__file__).parent.parent / "migrator.js").read_text()
 app = Application.app.server
 
 if __name__ == "__main__":
-    Application.app.renderer = (pathlib.Path(__file__).parent.parent / "migrator.js").read_text()
     Application.app.run(port=6969, dev_tools_ui=True, debug=True, host="127.0.0.1")
